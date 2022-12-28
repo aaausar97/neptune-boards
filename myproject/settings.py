26b4c332e38a0155b9f2e3a33be7d4ade140298b
Ausar Mundra <ausar@marine-snow.co>
2022-12-28 16:29:11 -0500

    updated settings, static root

--- myproject/settings.py
+++ myproject/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join('../', PROJECT_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
